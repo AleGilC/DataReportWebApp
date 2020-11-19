83b6766cdfcdb31b13e35aec41922a101c6190dc
Alejandro <gilc.alejandro@gmail.com>
2020-11-19 19:01:44 -0400

    Account Controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.UserName.ToLower())) return BadRequest("User is taken!!");
+            
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDTO.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string userName) => await _context.Users.AnyAsync(x => x.UserName.Equals(userName));
+    }
+}
---
+++ API/Data/Migrations/20201119214545_ExtendingUsers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendingUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                schema: "Security",
+                table: "tbl_AppUser",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                schema: "Security",
+                table: "tbl_AppUser",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                schema: "Security",
+                table: "tbl_AppUser");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                schema: "Security",
+                table: "tbl_AppUser");
+        }
+    }
+}
